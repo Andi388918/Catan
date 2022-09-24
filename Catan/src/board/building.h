3924a503ed6904f16369ed393a9a63d46b69f2b6
Andreas Schwaiger <Schwaiger1820@gmail.com>
2022-09-24 18:21:58 +0200

    added building type

--- Catan/src/board/building.h
+++ Catan/src/board/building.h
@@ -4,7 +4,7 @@ class Building
 {
 public:
 	enum class Type { Settlement, City, Road };
-	Building(std::size_t owned_by_player_index) : owned_by_player_index { owned_by_player_index }, type { type }  {}
+	Building(std::size_t owned_by_player_index, Type type) : owned_by_player_index { owned_by_player_index }, type { type }  {}
 
 private:
 	std::size_t owned_by_player_index;
